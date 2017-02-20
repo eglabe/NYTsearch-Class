63488d3c68c40209848c50c0d1319e6d13482bf8
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-02-19 18:05:57 -0800

    Javascript file

--- newyork.js
+++ newyork.js
@@ -1,20 +1,81 @@
+var searchTerm = "";
+var searchCount = 0;
+var startYear = 0;
+var endYear = 0;
+
+var articleCounter = 0;
+
+var authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
+
+var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+  authKey + "&q=";
+
+function runQuery(nyURL,count){
+	$.ajax({
+		url: nyURL,
+		method: 'GET',
+	}).done(function(NTdata) {
+		console.log(nyURL, NTdata);
+		
+
+		for(var i = 0; i < count; i++){
+
+			articleCounter++;
+
+			var article = $("<div>");
+			article.attr("id", "article-id-"+articleCounter);
+			$("#results-display").append(article);
+
+			if(NTdata.response.docs[i].headline !== 'null'){
+			 	$("#article-id-" + articleCounter).append("<h3> " + articleCounter + " " + NTdata.response.docs[i].headline.main +"</h3>");
+			}
+
+			if(NTdata.response.docs[i].byline && NTdata.response.docs[i].byline.original){
+				$("#article-id-" + articleCounter).append("<h5> " + NTdata.response.docs[i].byline.original + "</h5>");
+			}
+
+			if(NTdata.response.docs[i].snippet){
+				$("#article-id-" + articleCounter).append("<span> " + NTdata.response.docs[i].snippet + "</span>");
+			}
+
+			if(NTdata.response.docs[i].web_url){
+				$("#article-id-" + articleCounter).append("<br>URL: <a href='" + NTdata.response.docs[i].web_url + "'>" + NTdata.response.docs[i].web_url + "</a>");
+			}
+		}
+	});
+}	
 
 $("#searchBtn").on("click", function() {
 
-	var searchTerm = "google";
-	var Searchcount = "10";
-	var startYear = "2014";
-	var endYear = "2017";
+	//Empty the section where results to be displayed
+	$("#results-display").empty();
 
-	var authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
-	var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
- authKey + "&q=" +searchTerm ;
+	//Set article count to zero
+	articleCounter = 0;
 
- 	$.ajax({
-		url: queryURLBase,
-		method: 'GET',
-	}).done(function(response) {
-		console.log(response);
-	});
+
+	//Get user inputs from the form and assign to the variables
+	searchTerm = $("#searchTerm").val().trim();
+
+	//Append search term to the url query
+	queryURL = queryURLBase + searchTerm;
+
+
+	searchCount = $("#number").val().trim();
+	startYear = $("#startYear").val().trim();
+	endYear = $("#endYear").val().trim();
+
+	//If user inputs start year then add it to the query
+	if (parseInt(startYear)) {
+    	queryURL = queryURL + "&begin_date=" + startYear + "0101";
+  	}
+
+  	//If user inputs end year then add it to the query
+  	if (parseInt(endYear)) {
+    	queryURL = queryURL + "&end_date=" + endYear + "0101";
+  	}
+
+  	//pass final url query and number of article to the function
+  	runQuery(queryURL,searchCount);
 
 });
